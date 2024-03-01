e1b1a183aacb717924c1a7c4aa268ecbc6a80491
Alexey-GM <gaidaialexey0502@gmail.com>
2024-03-02 01:35:47 +0700

    dev: initial setup

--- app/src/main/java/com/example/flow/ui/locations/LocationsFragment.kt
+++ app/src/main/java/com/example/flow/ui/locations/LocationsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.flow.ui.notifications
+package com.example.flow.ui.locations
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.flow.databinding.FragmentNotificationsBinding
+import com.example.flow.databinding.FragmentLocationsBinding
 
-class NotificationsFragment : Fragment() {
+class LocationsFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentLocationsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val loactionsViewModel =
+            ViewModelProvider(this).get(LoactionsViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentLocationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        loactionsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
